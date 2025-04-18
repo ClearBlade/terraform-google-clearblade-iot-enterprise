36a12048d7b806347ab2f8074b6b4eba85933368
Matthew Walowski <mattwalowski@gmail.com>
2025-04-18 08:56:02 -0500

    hardcode to 0 nodes

--- gke.tf
+++ gke.tf
@@ -5,9 +5,8 @@ data "google_container_engine_versions" "cluster_version" {
 data "google_client_config" "provider" {}
 
 data "google_container_cluster" "primary" {
-  count    = var.create_gke_cluster ? 0 : 1
-  name     = var.gke_cluster_name == "" ? "${var.project_id}-primary-cluster" : var.gke_cluster_name
-  location = var.region
+  name       = var.gke_cluster_name == "" ? "${var.project_id}-primary-cluster" : var.gke_cluster_name
+  depends_on = [ google_container_cluster.primary ]
 }
 
 resource "google_container_cluster" "primary" {
@@ -41,10 +40,10 @@ resource "google_container_cluster" "primary" {
 }
 
 resource "google_container_node_pool" "primary_nodes" {
-  for_each = var.create_gke_node_pool ? { "primary" = true } : {}
-  name     = var.gke_node_pool_name == "" ? "${var.project_id}-primary-pool" : var.gke_node_pool_name
-  location = var.region
-  cluster  = var.create_gke_cluster ? google_container_cluster.primary[0].name : data.google_container_cluster.primary[0].name
+  count              = 0
+  name               = var.gke_node_pool_name == "" ? "${var.project_id}-primary-pool" : var.gke_node_pool_name
+  location           = var.region
+  cluster            = data.google_container_cluster.primary.name
   initial_node_count = var.initial_node_count
 
   autoscaling {
