00391798632bfdb1e5ffd960d642c6d0b80f6a04
Matthew Walowski <mattwalowski@gmail.com>
2025-04-18 10:47:50 -0500

    move options into clearblade block

--- helm.tf
+++ helm.tf
@@ -86,8 +86,6 @@ data "clearblade-google_helm_values" "cb_helm_values" {
         request_memory = var.helm_values.clearblade_request_memory
         limit_cpu = var.helm_values.clearblade_limit_cpu
         limit_memory = var.helm_values.clearblade_limit_memory
-    }
-    license = {
         renewal_webhooks = var.helm_values.clearblade_license_renewal_webhooks
         metrics_webhooks = var.helm_values.clearblade_metrics_reporting_webhooks
     }
