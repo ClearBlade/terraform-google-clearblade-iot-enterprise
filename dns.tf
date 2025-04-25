8feb2ad5eea1508e261e1ec7b29be70062d8d859
Matthew Walowski <mattwalowski@gmail.com>
2025-04-25 14:27:26 -0500

    update dns vars

--- dns.tf
+++ dns.tf
@@ -1,7 +1,7 @@
 resource "cloudflare_dns_record" "domain" {
-    count = var.helm_values.base_url == "" ? 1 : 0
+    count = var.create_dns_record ? 1 : 0
     zone_id = var.cloudflare_zone_id
-    name    = var.namespace_name
+    name    = split(".", var.helm_values.base_url)[0]
     content   = var.helm_values.primary_ip == "" ? google_compute_address.cb_primary[0].address : var.helm_values.primary_ip
     type    = "A"
     ttl     = 3600
