4b2833de532f9d3aecf2df615c572e9fd71b0afd
Martin Pandola <martinpandola@gmail.com>
2025-06-18 14:38:08 -0500

    fix kubernetes block assignment

--- main.tf
+++ main.tf
@@ -31,7 +31,7 @@ provider "clearblade-google" {
 }
 
 provider "helm" {
-  kubernetes {
+  kubernetes = {
     host                   = "https://${data.google_container_cluster.primary.endpoint}"
     cluster_ca_certificate = base64decode(data.google_container_cluster.primary.master_auth.0.cluster_ca_certificate)
     token = data.google_client_config.provider.access_token
