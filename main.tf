23ef54ef3c401723533c4c1383d0e80b44da636b
Matthew Walowski <mattwalowski@gmail.com>
2025-06-27 11:10:35 -0500

    udpate cb tf module version

--- main.tf
+++ main.tf
@@ -10,7 +10,7 @@ terraform {
     }
     clearblade-google = {
       source = "ClearBlade/clearblade-google"
-      version = "0.2.15"
+      version = "0.2.16"
     }
     cloudflare = {
       source  = "cloudflare/cloudflare"
