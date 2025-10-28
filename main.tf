9cf2b79d0132b2e11947e2f7a778dd06bb0e9dd3
Matthew Walowski <mattwalowski@gmail.com>
2025-10-28 12:48:42 -0500

    bump clearblade-google version (#12)

--- main.tf
+++ main.tf
@@ -10,7 +10,7 @@ terraform {
     }
     clearblade-google = {
       source = "ClearBlade/clearblade-google"
-      version = "0.2.23"
+      version = "0.2.24"
     }
     cloudflare = {
       source  = "cloudflare/cloudflare"
